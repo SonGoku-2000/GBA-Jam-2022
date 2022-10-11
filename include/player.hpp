ba9401ab289e562b827cb97e32d84120ff166a9e
SonGoku-2000 <pablontxs@gmail.com>
2022-10-10 21:46:47 -0500

    update

---
+++ include/player.hpp
@@ -0,0 +1,13 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+class Player {
+    private:
+        /* data */
+    public:
+        Player();
+};
+
+#endif
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
-#include "bn_core.h"
 #include "bn_compare.h"
+#include "bn_core.h"
 
 int main() {
     bn::core::init();
+    
 
     while(true) {
         bn::core::update();
